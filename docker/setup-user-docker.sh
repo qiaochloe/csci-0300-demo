522aae854c7df72f85bdf651b88bfcb3ea13accd
Nicholas DeMarinis <ndemarinis@wpi.edu>
2024-01-24 11:57:36 -0500

    Removed unnecessary setup user script.

--- docker/setup-user-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-target_user="$1"
-
-# set up passwordless sudo for user cs300-user
-useradd -m -s /bin/bash cs300-user && \
-    echo "cs300-user ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/cs300-init
--- docker/setup-user-podman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-target_user="$1"
-
-# set up passwordless sudo for user cs300-user
-useradd -m -s /bin/bash $target_user && \
-    echo "$target_user ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/cs300-init
